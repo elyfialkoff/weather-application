fceeaae8860070c9f59112b65bc01b5121c00a3b
elyfialkoff <mouseen92@gmail.com>
2020-05-08 11:34:00 -0600

    modified to use input flags

--- app.js
+++ app.js
@@ -3,15 +3,27 @@ const dotenv = require('dotenv').config()
 
 const API_KEY = process.env.API_KEY;
 
-const location = process.argv[2]
 
-const url = `http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY}`;
+const processInputs = () => {
+  const inputs = {}
+  var index = process.argv.indexOf('-l')
+  if (index > 0){
+    inputs['location'] = process.argv[index + 1]
+  }
+  return inputs
+}
 
-if (!location) {
+const inputs = processInputs()
+
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${inputs.location}&units=metric&appid=${API_KEY}`;
+
+console.log(process.argv)
+
+if (!inputs.location) {
   return console.log('Please add a <city,country> as parameter')
 }
 
 request.get(url, (error, response, body) => {
   const data = JSON.parse(body)
-  console.log(`Its ${data.main.temp} in ${location}`)
+  console.log(`Its ${data.main.temp} in ${inputs.location}`)
 })
